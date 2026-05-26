5c80c762ed2d1fbfd74be3cf7bdb29cdd2540220
zalinadzh <z.dzhambotova@mail.ru>
2026-05-26 16:23:44 +0300

    Completed Etch-a-Sketch project

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const newDiv = document.querySelector('#container');
-let newBlock = document.createElement('block');
-newBlock.classList.add('block');
-newDiv.appendChild(newBlock);
 
-for (let i = 0; i < 256; i++) {
-    let newBlock = document.createElement('block');
-    newBlock.classList.add('block');
-    newDiv.appendChild(newBlock);
+
+function createGrid(size) {
+    const newDiv = document.querySelector('#container');
+    newDiv.innerHTML = '';
+    let squareSize = 100 / size;
+    for (let i = 0; i < size * size; i++) {
+        const newBlock = document.createElement('div');
+        newBlock.classList.add('block');
+        newBlock.style.flex = `0 0 ${squareSize}%`;
+        newBlock.style.height = `${squareSize}%`;
+        newBlock.addEventListener('mouseover', function() {
+            newBlock.style.backgroundColor = 'yellow';
+        });
+        newDiv.appendChild(newBlock)
+    }
 }
+createGrid(16);
+const btn = document.querySelector('#reset-button');
+btn.addEventListener('click', () => {
+    let input = prompt('Введите количество квадратов на сторону (макс. 100):');
+    let size = parseInt(input);
+    if (size > 0 && size <= 100) {
+        createGrid(size);
+    } else {
+        alert('Пожалуйста, введите число от 1 до 100!');
+    }
+});
